aec2215e554832d3abd3744ab3aef33067dfe7d2
so.ota <so.ota.biz@gmail.com>
2025-11-02 01:53:47 +0900

    ESLint・Prettier 設定

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,18 +1,22 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettierConfig from 'eslint-config-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Prettierとの競合を防ぐ（Prettierを優先）
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'eslint.config.mjs',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
