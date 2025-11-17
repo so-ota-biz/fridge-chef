6054ba7d2ad10dc112a6c32072962ebe9036b940
so.ota <so.ota.biz@gmail.com>
2025-11-17 21:30:21 +0900

    指摘対応

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -4,11 +4,6 @@ import request from 'supertest'
 import { App } from 'supertest/types'
 import { AppModule } from '../src/app.module'
 
-// Jest globals declaration
-declare function describe(name: string, fn: () => void): void
-declare function beforeEach(fn: () => void | Promise<void>): void
-declare function it(name: string, fn: () => any): void
-
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>
 
